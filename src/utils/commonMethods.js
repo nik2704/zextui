b04035a6a108c769bec155286f78558663adb49f
Nikolay Zenchenko <zenchenko.nikolay@microfocus.com>
2022-02-24 11:09:42 +0300

    24.02.2022-2

--- src/utils/commonMethods.js
+++ src/utils/commonMethods.js
@@ -56,6 +56,7 @@ export function fetchSMAXData ( fetchParams ) {
   }).catch( err => {
     console.log(`ERROR fetching data: `);
     console.log(fetchParams);
+    console.log(err);
     return null;
   });
 }
